dff847e1794978222838233527454b8d426b7fcf
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2025-11-14 19:49:28 +0500

    Config settings

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.configs.settings import get_settings
+
+settings = get_settings()
+app = FastAPI(title=settings.APP_NAME)
 
 
 @app.get("/")
@@ -9,6 +12,5 @@ def read_root():
     return {"message": "Hello World"}
 
 
-
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", loop="uvloop", http="httptools", reload=True)
+    uvicorn.run("app.main:app", loop="uvloop", http="httptools", reload=False)
